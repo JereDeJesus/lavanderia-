9b4724ab8012a2808e30dc8f8caacd705ead7729
JereDeJesus <jeremaya.halu@gmail.com>
2024-12-07 11:17:51 -0400

    Update contacto.js

--- models/contacto.js
+++ models/contacto.js
@@ -1,11 +1,11 @@
-// models/Contacto.js
 const mongoose = require('mongoose');
 
 const contactoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  correo: { type: String, required: true },
-  comentario: { type: String, required: true },
-  fecha: { type: Date, default: Date.now }
+    nombre: { type: String, required: true },
+    correo: { type: String, required: true },
+    comentario: { type: String, required: true },
+    direccion_ip: { type: String, required: true },
+    fecha_hora: { type: Date, default: Date.now }
 });
 
 const Contacto = mongoose.model('Contacto', contactoSchema);
